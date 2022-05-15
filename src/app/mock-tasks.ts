4d70a2aacfbd795b92e00af0daa3d60371df65a1
Dushyantha Kalehewatta <Ulindu.Dushyantha@softlogiclife.lk>
2022-05-15 16:11:10 +0530

    add current files

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS : Task []= [
+{
+    id :1 , 
+    text : 'Doctor Appointment ', 
+    day : 'May 5th at 2:30PM', 
+    reminder : true
+}, 
+{
+    id :2 , 
+    text : 'Appointment 2', 
+    day : 'May 6th at 1:30PM', 
+    reminder : true
+}, 
+{
+    id :3 , 
+    text : 'Appointment 3', 
+    day : 'May 8th at 3:30PM', 
+    reminder : false
+}
+];
